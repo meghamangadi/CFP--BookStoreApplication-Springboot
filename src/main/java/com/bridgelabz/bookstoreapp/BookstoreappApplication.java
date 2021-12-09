ccbe9ab05437d5f8bca4a804269549b3bb5367a9
megha angadi <meghamangadi@gmail.com>
2021-12-09 18:03:30 +0530

    [Megha] Added : Forgetpassword API

--- src/main/java/com/bridgelabz/bookstoreapp/BookstoreappApplication.java
+++ src/main/java/com/bridgelabz/bookstoreapp/BookstoreappApplication.java
@@ -9,7 +9,7 @@ public class BookstoreappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreappApplication.class, args);
 	
-		System.out.println("Welcome to Bookstore app");
+		
 		}
 
 }
